41e2cad612196e09255477acf3a8249b460e2fe6
Nicola Masella <nmasella@gmail.com>
2020-12-30 14:54:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sapper-twin-app-example
+ Example of use of Twind in Sapper ufficial Boilerplate
---
+++ src/routes/about.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { tw, setup } from 'twind';
+
+	setup({
+  		hash: true,
+	});
+
+	const styles = {
+		center: tw`flex items-center justify-center`,
+		bgcolor: tw`bg-indigo-400`
+	}
+</script>
+
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<h1>About this site</h1>
+
+<p class="{tw `${styles.bgcolor} ${styles.center} text-white`}">This is the 'about' page. There's not much here.</p>
